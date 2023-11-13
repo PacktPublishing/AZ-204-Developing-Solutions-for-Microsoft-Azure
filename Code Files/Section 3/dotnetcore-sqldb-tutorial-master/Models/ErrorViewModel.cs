811d70bd1f4cc2cd2208765e118046eff9477dae
ClaraJosephPackt <68842996+ClaraJosephPackt@users.noreply.github.com>
2023-11-13 16:51:49 +0530

    Delete Code Files directory

--- Code Files/Section 3/dotnetcore-sqldb-tutorial-master/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNetCoreSqlDb.Models
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; } = default!;
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- Code Files/Section 5/durablefunctions-apiscraping-dotnet-master/CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-## durablefunctions-apiscraping-dotnet Changelog
-
-<a name="x.y.z"></a>
-# 2018-08-31
-
-Created repository.
